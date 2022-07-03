90b8b5544131501a4028992ba2dadc791705043c
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-03 18:02:35 -0300

    feat: create user model

---
+++ src/database/models/user.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      tableName: 'Users',
+    },
+  );
+
+  return User;
+};
