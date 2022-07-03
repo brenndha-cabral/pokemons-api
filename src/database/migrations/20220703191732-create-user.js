e19b1213c8cdeeeac2ef985acbf80c009b9561f9
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-03 19:57:45 -0300

    feat: fix user model

--- src/database/migrations/20220703191732-create-user.js
+++ src/database/migrations/20220703191732-create-user.js
@@ -19,6 +19,14 @@ module.exports = {
       image: {
         type: Sequelize.STRING,
       },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
     });
   },
   async down(queryInterface, _Sequelize) {
