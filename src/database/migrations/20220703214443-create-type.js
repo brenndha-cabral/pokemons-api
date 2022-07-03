57072875798b1abf28e5089847c57434b1688146
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-03 19:58:25 -0300

    feat: create type migration

---
+++ src/database/migrations/20220703214443-create-type.js
@@ -0,0 +1,26 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Types', {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      typePokemon: {
+        type: Sequelize.STRING,
+        field: 'type_pokemon',
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Types');
+  },
+};
